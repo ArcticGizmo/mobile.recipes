1500d744bb01b2787c7a2a1524e9054f382661d8
ArcticGizmo <jon.howell314@gmail.com>
2025-02-03 21:16:04 +0800

    Added reorder to steps

--- src/components/inputs/TextareaArrayInput.vue
+++ src/components/inputs/TextareaArrayInput.vue
@@ -3,19 +3,25 @@
     <div class="mb-2">
       <IonLabel>{{ label }}</IonLabel>
     </div>
-    <div class="flex items-start mb-4" v-for="(field, index) of fields" :key="index">
-      <TextareaInput :name="`${name}[${index}]`" :label="`Step ${index + 1}`" />
-      <IonButton fill="clear" @click="onRemove(index, !!field.value)">
-        <IonIcon slot="icon-only" :icon="close" color="danger" />
-      </IonButton>
-    </div>
+    <IonList>
+      <IonReorderGroup :disabled="false" @ion-item-reorder="onReorder">
+        <IonItem v-for="(field, index) of fields" :key="index" lines="none">
+          <IonReorder class="-ml-4 mr-2" slot="start" />
+          <TextareaInput class="my-2" :name="`${name}[${index}]`" :label="`Step ${index + 1}`" />
+          <IonButton slot="end" fill="clear" @click="onRemove(index, !!field.value)">
+            <IonIcon slot="icon-only" :icon="close" color="danger" />
+          </IonButton>
+        </IonItem>
+      </IonReorderGroup>
+    </IonList>
 
     <IonButton @click="push('')">Add Step</IonButton>
   </div>
 </template>
 
 <script setup lang="ts">
-import { alertController, IonButton, IonIcon, IonLabel } from '@ionic/vue';
+import { alertController, IonButton, IonIcon, IonItem, IonLabel, IonList, IonReorder, IonReorderGroup } from '@ionic/vue';
+import { IonReorderGroupCustomEvent, ItemReorderEventDetail } from '@ionic/core';
 import { useFieldArray } from 'vee-validate';
 import TextareaInput from './TextareaInput.vue';
 import { close } from '@/icons';
@@ -25,7 +31,7 @@ const props = defineProps<{
   label: string;
 }>();
 
-const { remove, push, fields } = useFieldArray(() => props.name);
+const { remove, push, fields, swap } = useFieldArray(() => props.name);
 
 const onRemove = async (index: number, hasData: boolean) => {
   if (!hasData) {
@@ -53,6 +59,11 @@ const onRemove = async (index: number, hasData: boolean) => {
   if (resp.role === 'destructive') {
     remove(index);
   }
+};
+
+const onReorder = (e: IonReorderGroupCustomEvent<ItemReorderEventDetail>) => {
+  swap(e.detail.from, e.detail.to);
+  e.detail.complete(fields.value);
 };
 </script>
 
